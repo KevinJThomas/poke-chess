f3aab3e907bf06877ddfd9c1c5099a82f1c3ed13
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-01-23 13:50:30 -0700

    Add drag pokemon

--- pokechess.client/src/main.js
+++ pokechess.client/src/main.js
@@ -1,109 +1,169 @@
 import kaplay from 'kaplay';
-import 'kaplay/global'; // uncomment if you want to use without the k. prefix
-
-const FLOOR_HEIGHT = 48;
-const JUMP_FORCE = 800;
-const SPEED = 480;
+// import 'kaplay/global'; // uncomment if you want to use without the k. prefix
 
 // initialize context
-kaplay();
+const k = kaplay();
 
 // load assets
-loadSprite('bean', 'sprites/bean.png');
+k.loadSprite('pokemon', 'sheet/pokemon.png', {
+  sliceX: 12, // how many sprites are in the X axis
+  sliceY: 13, // how many sprites are in the Y axis
+  // anims: {
+  //   crack: { from: 0, to: 3, loop: false },
+  //   ghosty: { from: 4, to: 4 },
+  // },
+});
 
-scene('game', () => {
-  // define gravity
-  setGravity(1600);
+// k.add([
+//   // list of components
+//   sprite('pokemon', { frame: 0 }),
+//   pos(80, 40),
+//   area(),
+//   body(),
+// ]);
 
-  // add a game object to screen
-  const player = add([
+for (let i = 0; i < 10; i++) {
+  k.add([
     // list of components
-    sprite('bean'),
-    pos(80, 40),
+    sprite('pokemon', { frame: i }),
+    pos(i * 128 + 128 / 2, 128),
+    anchor('center'),
     area(),
-    body(),
+    'pokemon',
   ]);
+}
 
-  // floor
-  add([
-    rect(width(), FLOOR_HEIGHT),
-    outline(4),
-    pos(0, height()),
-    anchor('botleft'),
-    area(),
-    body({ isStatic: true }),
-    color(127, 200, 255),
-  ]);
+// foo.onClick(() => {
+//   console.log('clicked', i);
+// });
+// }
 
-  function jump() {
-    if (player.isGrounded()) {
-      player.jump(JUMP_FORCE);
-    }
+// k.onClick('pokemon', (pokemon) => {
+//   console.log('clicked', pokemon);
+// });
+
+k.onHoverUpdate('pokemon', (pokemon) => {
+  if (k.isMouseDown('left')) {
+    pokemon.pos = k.mousePos();
   }
-
-  // jump when user press space
-  onKeyPress('space', jump);
-  onClick(jump);
-
-  function spawnTree() {
-    // add tree obj
-    add([
-      rect(48, rand(32, 96)),
-      area(),
-      outline(4),
-      pos(width(), height() - FLOOR_HEIGHT),
-      anchor('botleft'),
-      color(255, 180, 255),
-      move(LEFT, SPEED),
-      'tree',
-    ]);
-
-    // wait a random amount of time to spawn next tree
-    wait(rand(0.5, 1.5), spawnTree);
-  }
-
-  // start spawning trees
-  spawnTree();
-
-  // lose if player collides with any game obj with tag "tree"
-  player.onCollide('tree', () => {
-    // go to "lose" scene and pass the score
-    go('lose', score);
-    burp();
-    addKaboom(player.pos);
-  });
-
-  // keep track of score
-  let score = 0;
-
-  const scoreLabel = add([text(score), pos(24, 24)]);
-
-  // increment score every frame
-  onUpdate(() => {
-    score++;
-    scoreLabel.text = score;
-  });
+  console.log('hover', pokemon);
 });
 
-scene('lose', (score) => {
-  add([
-    sprite('bean'),
-    pos(width() / 2, height() / 2 - 80),
-    scale(2),
-    anchor('center'),
-  ]);
+// k.onHoverUpdate('pokemon', (pokemon) => {
+//   console.log('hover-update', pokemon);
+// });
 
-  // display score
-  add([
-    text(score),
-    pos(width() / 2, height() / 2 + 80),
-    scale(2),
-    anchor('center'),
-  ]);
+// k.onHoverEnd('pokemon', (pokemon) => {
+//   console.log('hover-end', pokemon);
+// });
 
-  // go back to game with space is pressed
-  onKeyPress('space', () => go('game'));
-  onClick(() => go('game'));
-});
+// k.onMouseDown('left', () => {
+//   pokemon.pos = k.mousePos();
+// });
 
-go('game');
+// k.onMouseMove((pos) => {
+//   console.log('mouse-move', pos);
+// });
+
+// k.onUpdate(() => {
+//   console.log(k.isMouseDown('left'));
+// });
+
+// k.onMousePress(() => console.log('mouse-press'));
+
+// scene('game', () => {
+//   // define gravity
+//   setGravity(1600);
+
+//   // add a game object to screen
+//   const player = add([
+//     // list of components
+//     sprite('pokemon', { frame: 0 }),
+//     pos(80, 40),
+//     area(),
+//     body(),
+//   ]);
+
+//   // floor
+//   add([
+//     rect(width(), FLOOR_HEIGHT),
+//     outline(4),
+//     pos(0, height()),
+//     anchor('botleft'),
+//     area(),
+//     body({ isStatic: true }),
+//     color(127, 200, 255),
+//   ]);
+
+//   function jump() {
+//     if (player.isGrounded()) {
+//       player.jump(JUMP_FORCE);
+//     }
+//   }
+
+//   // jump when user press space
+//   onKeyPress('space', jump);
+//   onClick(jump);
+
+//   function spawnTree() {
+//     // add tree obj
+//     add([
+//       rect(48, rand(32, 96)),
+//       area(),
+//       outline(4),
+//       pos(width(), height() - FLOOR_HEIGHT),
+//       anchor('botleft'),
+//       color(255, 180, 255),
+//       move(LEFT, SPEED),
+//       'tree',
+//     ]);
+
+//     // wait a random amount of time to spawn next tree
+//     wait(rand(0.5, 1.5), spawnTree);
+//   }
+
+//   // start spawning trees
+//   spawnTree();
+
+//   // lose if player collides with any game obj with tag "tree"
+//   player.onCollide('tree', () => {
+//     // go to "lose" scene and pass the score
+//     go('lose', score);
+//     burp();
+//     addKaboom(player.pos);
+//   });
+
+//   // keep track of score
+//   let score = 0;
+
+//   const scoreLabel = add([text(score), pos(24, 24)]);
+
+//   // increment score every frame
+//   onUpdate(() => {
+//     score++;
+//     scoreLabel.text = score;
+//   });
+// });
+
+// scene('lose', (score) => {
+//   add([
+//     sprite('bulbasaur'),
+//     pos(width() / 2, height() / 2 - 80),
+//     scale(2),
+//     anchor('center'),
+//   ]);
+
+//   // display score
+//   add([
+//     text(score),
+//     pos(width() / 2, height() / 2 + 80),
+//     scale(2),
+//     anchor('center'),
+//   ]);
+
+//   // go back to game with space is pressed
+//   onKeyPress('space', () => go('game'));
+//   onClick(() => go('game'));
+// });
+
+// go('game');
