9fdc01ff08b46c16b576ec44920eb08b719b1d2f
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-01-26 18:11:36 -0700

    Add attack arrow

--- pokechess.client/src/components/PlayerPokemon.jsx
+++ pokechess.client/src/components/PlayerPokemon.jsx
@@ -3,13 +3,13 @@ import Pokemon from "./Pokemon";
 export default function PlayerPokemon() {
   return (
     <>
-      <Pokemon name="Pikachu" attack={10} health={100} />
-      <Pokemon name="Pikachu" attack={10} health={100} />
-      <Pokemon name="Pikachu" attack={10} health={100} />
-      <Pokemon name="Pikachu" attack={10} health={100} />
-      <Pokemon name="Pikachu" attack={10} health={100} />
-      <Pokemon name="Pikachu" attack={10} health={100} />
-      <Pokemon name="Pikachu" attack={10} health={100} />
+      <Pokemon name="Pikachu" attack={10} health={100} id={1} />
+      <Pokemon name="Pikachu" attack={10} health={100} id={2} />
+      <Pokemon name="Pikachu" attack={10} health={100} id={3} />
+      <Pokemon name="Pikachu" attack={10} health={100} id={4} />
+      <Pokemon name="Pikachu" attack={10} health={100} id={5} />
+      <Pokemon name="Pikachu" attack={10} health={100} id={6} />
+      <Pokemon name="Pikachu" attack={10} health={100} id={7} />
     </>
   );
 }
