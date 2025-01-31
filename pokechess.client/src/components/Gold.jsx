983a84fb940e7b834f69e75c5dffed02fb2cd98b
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-01-31 13:46:08 -0700

    Update gold style

--- pokechess.client/src/components/Gold.jsx
+++ pokechess.client/src/components/Gold.jsx
@@ -1,6 +1,6 @@
 export default function Gold({ gold, maxGold }) {
   return (
-    <div className="absolute right-0 bottom-0 flex h-6 w-18 items-center justify-center rounded-xl bg-amber-400">
+    <div className="absolute right-0 bottom-0 flex h-6 w-18 items-center justify-center rounded-tl-md bg-amber-400">
       <span className="text-nowrap">
         {gold} / {maxGold}
       </span>
