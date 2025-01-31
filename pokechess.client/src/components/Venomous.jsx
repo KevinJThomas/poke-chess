d8953476368eb62e718c518587f5a0318f73f777
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-01-31 12:51:48 -0700

    Move venom

--- pokechess.client/src/components/Venomous.jsx
+++ pokechess.client/src/components/Venomous.jsx
@@ -2,7 +2,7 @@ import { BeakerIcon } from "@heroicons/react/24/solid";
 
 export default function Venomous() {
   return (
-    <div className="absolute bottom-0 left-[28px] z-5 flex h-6 w-6 items-center justify-center rounded-full bg-lime-500">
+    <div className="absolute top-0 left-[28px] z-5 flex h-6 w-6 items-center justify-center rounded-full bg-lime-500">
       <BeakerIcon className="size-4 text-lime-900" />
     </div>
   );
