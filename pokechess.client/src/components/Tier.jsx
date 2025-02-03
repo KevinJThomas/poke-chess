6438d5b00f6473fe8270299396f31ea8890330ff
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-02-02 22:16:01 -0700

    Stuff

--- pokechess.client/src/components/Tier.jsx
+++ pokechess.client/src/components/Tier.jsx
@@ -1,7 +1,9 @@
+import { toRoman } from "../util";
+
 export default function Tier({ tier }) {
   return (
     <div className="absolute top-0 left-0 z-5 flex h-6 w-6 items-center justify-center rounded-full bg-purple-400">
-      <span className="text-sm">{tier}</span>
+      <span className="text-sm">{toRoman(tier)}</span>
     </div>
   );
 }
