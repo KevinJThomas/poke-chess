415e8fd19e43374a6580f2d91171b3b8c17a2473
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-02-02 22:09:31 -0700

    Critical

--- pokechess.client/src/components/HeroDamage.jsx
+++ pokechess.client/src/components/HeroDamage.jsx
@@ -1,6 +1,6 @@
 export default function HeroDamage({ damage }) {
   return (
-    <div className="absolute bottom-[24px] left-[24px] flex h-12 w-12 items-center justify-center rounded-full border-2 bg-amber-400">
+    <div className="animate-jump-in absolute bottom-[24px] left-[24px] flex h-12 w-12 items-center justify-center rounded-full border-2 bg-amber-400">
       <span className="text-xl font-bold">-{damage}</span>
     </div>
   );
