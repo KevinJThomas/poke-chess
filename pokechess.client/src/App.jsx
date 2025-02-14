da308c1753bf28054bb733c2d9eedfbe248df60e
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-02-13 17:13:54 -0700

    Fix

--- pokechess.client/src/App.jsx
+++ pokechess.client/src/App.jsx
@@ -329,7 +329,7 @@ export default function App() {
       if (playerId) {
         setPlayerId(playerId);
       }
-      if (gameStatus === "name") {
+      if (gameStatus === "name" || gameStatus === "lobby") {
         setGameStatus("lobby");
       } else {
         setGameStatus("shop");
