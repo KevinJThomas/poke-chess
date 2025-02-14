69bfe6c4e40b805ac2849ddd87a21ea5b3fd9613
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-02-14 10:16:22 -0700

    Style changes

--- pokechess.client/src/components/Stealth.jsx
+++ pokechess.client/src/components/Stealth.jsx
@@ -1,5 +1,9 @@
-export default function Stealth() {
+import { EyeSlashIcon } from "@heroicons/react/24/solid";
+
+export default function Sealth() {
   return (
-    <div className="absolute bottom-0 left-0 flex h-20 w-20 items-center justify-center rounded-full bg-slate-800/40"></div>
+    <div className="absolute top-0 right-0 z-5 flex h-6 w-6 items-center justify-center rounded-full bg-fuchsia-900">
+      <EyeSlashIcon className="size-4 text-slate-200" />
+    </div>
   );
 }
