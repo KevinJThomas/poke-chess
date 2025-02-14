7df0921782f8a4376a414decfee5d14f3a06196a
KevinJThomas <kevinthomas2710@gmail.com>
2025-02-13 18:26:25 -0700

    Adding player's board to CombatStarted response.

--- PokeChess.Server/MessageHub.cs
+++ PokeChess.Server/MessageHub.cs
@@ -375,7 +375,7 @@ namespace PokeChess.Server
         {
             foreach (var player in lobby.Players)
             {
-                await Clients.Client(player.Id).SendAsync("CombatStarted", player.CombatActions, lobby.Players.Where(x => x.Id == player.PreviousOpponentId).FirstOrDefault().Board);
+                await Clients.Client(player.Id).SendAsync("CombatStarted", player.CombatActions, lobby.Players.Where(x => x.Id == player.PreviousOpponentId).FirstOrDefault().Board, player.Board);
             }
         }
     }
