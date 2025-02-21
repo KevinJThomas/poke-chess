b792cfb03d1ba22c0315149f8b235bd35160d2f8
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-02-21 13:54:27 -0700

    Here

--- pokechess.client/src/components/Button.jsx
+++ pokechess.client/src/components/Button.jsx
@@ -1,11 +1,11 @@
-import clsx from "clsx";
+import { cn } from "../util";
 export default function Button({ children, className, disabled, ...props }) {
   return (
     <button
       {...props}
       disabled={disabled}
       type="button"
-      className={clsx(
+      className={cn(
         "group inline-flex h-8 items-center justify-center overflow-hidden rounded-md border border-neutral-600 bg-amber-200 px-1 text-sm [box-shadow:0px_4px_1px_#a3a3a3]",
         disabled && "cursor-not-allowed opacity-50",
         !disabled &&
